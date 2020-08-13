77dcdfada6ecd7e0580d81477a0c9fe7b54b0d49
eunjung <jenny.it.kr@gmail.com>
2020-08-13 09:40:31 +0900

    refactor(api): delete from cart (#11)
    - Promise.all => for await of 문으로 변경
    - 응답 내용 변경

--- src/server/api/delete-from-cart.ts
+++ src/server/api/delete-from-cart.ts
@@ -3,24 +3,22 @@ import { prisma } from '../utils/prisma'
 
 const deleteFromCartRouter = express.Router()
 
-function deleteFromCart(userId: number, productId: number) {
-  return prisma.cart.delete({
-    where: {
-      userId_productId: {
-        userId,
-        productId,
-      },
-    },
-  })
-}
-
 deleteFromCartRouter.delete('/delete-from-cart', async (req, res) => {
   const userId = req.auth?.userId
 
   const productIds = req.body.productIds
 
-  const result = await Promise.all(productIds.map((productId) => deleteFromCart(userId, productId)))
-  res.json({ message: `${result.length} items deleted` })
+  for await (let productId of productIds) {
+    await prisma.cart.delete({
+      where: {
+        userId_productId: {
+          userId,
+          productId,
+        },
+      },
+    })
+  }
+  res.json(true)
 })
 
 export { deleteFromCartRouter }
