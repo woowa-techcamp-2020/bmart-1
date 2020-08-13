b11114b95de2c8b44cdb755270691d22b4d49854
jhaemin <io@jhaemin.com>
2020-08-13 22:58:01 +0900

    feat(api): get products by category (#20)

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { getUserInfoRouter } from './get-user-info'
 import { addAddressRouter } from './add-address'
+import { getProductsByCategoryRouter } from './get-products-by-category'
 
 const apiRouter = express.Router()
 
 apiRouter.use('/api', getUserInfoRouter)
 apiRouter.use('/api', addAddressRouter)
+apiRouter.use('/api', getProductsByCategoryRouter)
 
 export { apiRouter }
