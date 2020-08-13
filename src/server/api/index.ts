1f336699f8eebb3ea0c5e6ae0daf76a7ccf40f96
eunjung <jenny.it.kr@gmail.com>
2020-08-13 14:27:11 +0900

    chore: 라우터 추가

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,8 +1,16 @@
 import express from 'express'
 import { getUserInfoRouter } from './get-user-info'
+import { getProductsInCartRouter } from './get-products-in-cart'
+import { deleteFromCartRouter } from './delete-from-cart'
+import { setDefaultAddressRouter } from './set-default-address'
+import { searchRouter } from './search'
 
 const apiRouter = express.Router()
 
 apiRouter.use('/api', getUserInfoRouter)
+apiRouter.use('/api', getProductsInCartRouter)
+apiRouter.use('/api', deleteFromCartRouter)
+apiRouter.use('/api', setDefaultAddressRouter)
+apiRouter.use('/api', searchRouter)
 
 export { apiRouter }
