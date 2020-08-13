d55ded81db563f8f14cf0cfb997a65331e26b7f7
agrajak <korbots@gmail.com>
2020-08-13 20:49:04 +0900

    feat: add addToCartRouter (#15)

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import { getUserInfoRouter } from './get-user-info'
 import { getJjimsRouter } from './get-jjims'
 import { getProductsByTopicRouter } from './get-products-by-topic'
+import { addToCartRouter } from './add-to-cart'
 
 const apiRouter = express.Router()
 
 apiRouter.use('/api', getUserInfoRouter)
 apiRouter.use('/api', getJjimsRouter)
 apiRouter.use('/api', getProductsByTopicRouter)
+apiRouter.use('/api', addToCartRouter)
 
 export { apiRouter }
