9a7331aade78a19617b8fc9e3bde18f57a37fa44
agrajak <korbots@gmail.com>
2020-08-13 14:29:07 +0900

    feat: add getProductsByTopicRouter (#14) - topic이 new인 경우: 새로 등록된 상품 순서대로 반환 - topic이 now인 경우: 랜덤으로 10개 골라서 반환

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { getUserInfoRouter } from './get-user-info'
 import { getJjimsRouter } from './get-jjims'
+import { getProductsByTopicRouter } from './get-products-by-topic'
 
 const apiRouter = express.Router()
 
 apiRouter.use('/api', getUserInfoRouter)
 apiRouter.use('/api', getJjimsRouter)
+apiRouter.use('/api', getProductsByTopicRouter)
 
 export { apiRouter }
