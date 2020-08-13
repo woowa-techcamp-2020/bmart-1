5edbf1bfd57e0ff3c6645c1e31895fa2fe54f340
agrajak <korbots@gmail.com>
2020-08-13 13:21:22 +0900

    feat: add getJjimsRouter (#13)

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { getUserInfoRouter } from './get-user-info'
+import { getJjimsRouter } from './get-jjims'
 
 const apiRouter = express.Router()
 
 apiRouter.use('/api', getUserInfoRouter)
+apiRouter.use('/api', getJjimsRouter)
 
 export { apiRouter }
