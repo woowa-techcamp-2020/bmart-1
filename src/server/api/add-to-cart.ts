a9bf632c596c6b703569b6dd53f6e071b2b0af9c
agrajak <korbots@gmail.com>
2020-08-15 15:03:27 +0900

    chore: add body validation for /add-to-cart and error handling (#15, #27) - 상품이 존재하지 않는 경우에  예외 출력하도록 변경 - RequestBody Validation 추가

--- src/server/api/add-to-cart.ts
+++ src/server/api/add-to-cart.ts
@@ -1,39 +1,61 @@
-import { prisma } from '../utils/prisma'
-import express, { Request, Response } from 'express'
 import { CartCreateInput, CartUpdateInput } from '@prisma/client'
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
 import { STATUS_CODE } from '~/../constants'
+import { requestValidator } from '~/middlewares'
+import { prisma } from '../utils/prisma'
+
+type AddToCartBodyRequest = {
+  productId: number
+  quantity: number
+}
+
 const addToCartRouter = express.Router()
-addToCartRouter.post('/add-to-cart', async (req: Request, res: Response) => {
-  const userId = req.auth?.userId as number
-  const { productId, quantity } = req.body
-  try {
-    const query: CartCreateInput | CartUpdateInput = {
-      user: {
-        connect: {
-          id: userId,
-        },
-      },
-      product: {
-        connect: {
+
+addToCartRouter.post(
+  '/add-to-cart',
+  [body('productId').isInt(), body('quantity').isInt({ min: 1 })],
+  requestValidator(),
+  async (req: Request<{}, {}, AddToCartBodyRequest>, res: Response) => {
+    const userId = req.auth?.userId as number
+    const { productId, quantity } = req.body
+
+    try {
+      const product = await prisma.product.findOne({
+        where: {
           id: productId,
         },
-      },
-      quantity,
-    }
-    const cart = await prisma.cart.upsert({
-      create: query as CartCreateInput,
-      update: query as CartUpdateInput,
-      where: {
-        userId_productId: {
-          userId,
-          productId,
+      })
+      if (!product) throw new Error('존재하지 않는 상품입니다.')
+
+      const query: CartCreateInput | CartUpdateInput = {
+        user: {
+          connect: {
+            id: userId,
+          },
         },
-      },
-    })
-    res.status(STATUS_CODE.OK)
-  } catch (e) {
-    res.status(STATUS_CODE.BAD_REQUEST).send({ message: e.message })
+        product: {
+          connect: {
+            id: productId,
+          },
+        },
+        quantity,
+      }
+      await prisma.cart.upsert({
+        create: query as CartCreateInput,
+        update: query as CartUpdateInput,
+        where: {
+          userId_productId: {
+            userId,
+            productId,
+          },
+        },
+      })
+      res.status(STATUS_CODE.OK)
+    } catch (e) {
+      res.status(STATUS_CODE.BAD_REQUEST).send({ message: e.message })
+    }
   }
-})
+)
 
 export { addToCartRouter }
