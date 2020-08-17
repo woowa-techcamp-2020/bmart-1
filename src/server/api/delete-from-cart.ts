8b55165ce19191e04bc2aace15ef7fcf374e8cfe
eunjung <jenny.it.kr@gmail.com>
2020-08-17 11:16:38 +0900

    refactor(api): delete-from-cart (#11)

--- src/server/api/delete-from-cart.ts
+++ src/server/api/delete-from-cart.ts
@@ -1,6 +1,8 @@
 import express, { Request } from 'express'
 import { prisma } from '../utils/prisma'
 import { STATUS_CODE, ERROR_MSG } from '~/../constants'
+import { body } from 'express-validator'
+import { requestValidator } from '~/middlewares'
 
 const deleteFromCartRouter = express.Router()
 
@@ -8,22 +10,30 @@ type DeleteFromCartBody = {
   productIds: number[]
 }
 
+function productIdsValidator(ids: number[]) {
+  return ids.every((id) => typeof id === 'number')
+}
+
 deleteFromCartRouter.delete(
   '/delete-from-cart',
+  body('productIds').isArray({ min: 1 }).custom(productIdsValidator),
+  requestValidator(),
   async (req: Request<{}, {}, DeleteFromCartBody>, res) => {
     const userId = (req.auth?.userId as number) ?? 15
     const productIds = req.body.productIds
 
     const queryString = `
-    DELETE FROM cart WHERE (userId, productId) IN (${productIds.map(
-      (productId) => `(${userId}, ${productId})`
-    )})
+    DELETE FROM cart WHERE (userId, productId) IN (${productIds
+      .map((productId) => `(${userId}, ${productId})`)
+      .join(',')})
     `
 
     try {
       await prisma.$queryRaw(queryString)
       res.sendStatus(STATUS_CODE.OK)
     } catch (e) {
+      console.error(e)
+
       res.status(STATUS_CODE.INTERNAL_ERROR).send({ message: ERROR_MSG.INTERNAL_ERROR })
     }
   }
