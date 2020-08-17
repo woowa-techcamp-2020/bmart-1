b6f64e16e25efb759d2f2ad789648df8c3614094
jhaemin <io@jhaemin.com>
2020-08-17 16:59:58 +0900

    feat: fill carousel components (#56)

---
+++ src/components/Carousel/AdBannerContainer/AdBanner/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.scss'
+
+export type AdBannerProps = unknown
+
+const AdBanner: React.FC<AdBannerProps> = (props) => {
+  return <div className=""></div>
+}
+
+export default AdBanner
---
+++ src/components/Carousel/Dots/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.scss'
+
+export type DotsProps = unknown
+
+const Dots: React.FC<DotsProps> = (props) => {
+  return <div className=""></div>
+}
+
+export default Dots
