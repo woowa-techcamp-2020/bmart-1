131d420fecf6a500d09f64905564a6d0b6feb152
jhaemin <io@jhaemin.com>
2020-08-17 16:59:27 +0900

    feat: create carousel component (#56)

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.scss'
+
+export type CarouselProps = unknown
+
+const Carousel: React.FC<CarouselProps> = (props) => {
+  return <div className="carousel"></div>
+}
+
+export default Carousel
