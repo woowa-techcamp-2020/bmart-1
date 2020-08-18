45dc76fbcf4b317b061f2c67063170b969f6030d
agrajak <korbots@gmail.com>
2020-08-18 17:18:37 +0900

    feat(storybook): add OptionSelector story for storybook

---
+++ src/stories/OptionSelector.stories.tsx
@@ -0,0 +1,23 @@
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Meta, Story } from '@storybook/react/types-6-0'
+import React from 'react'
+import { SORT_BY_TYPES } from 'src/constants'
+import OptionSelector, { OptionSelectorProps } from '../pages/CategoryDetails/OptionSelector'
+
+export default {
+  title: 'CategoryDetails/OptionSelector',
+  component: OptionSelector,
+  args: {
+    options: SORT_BY_TYPES.slice(),
+    optionIdx: 0,
+  },
+  argTypes: {
+    optionIdx: {
+      control: { type: 'range', min: 0, max: SORT_BY_TYPES.length, step: 1 },
+    },
+  },
+} as Meta
+
+const Template: Story<OptionSelectorProps> = (args) => <OptionSelector {...args} />
+
+export const Main = Template.bind({})
