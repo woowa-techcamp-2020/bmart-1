c510d4af5b63f4d9ba38b16a9c35d6e6cdbc23ad
agrajak <korbots@gmail.com>
2020-08-19 20:58:57 +0900

    feat: add mix-blend-mode and make selector movable - 이제 selector가 날라다닌다.

--- src/pages/CategoryDetails/OptionSelector/index.tsx
+++ src/pages/CategoryDetails/OptionSelector/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { DrawerContext } from 'src/components/Drawer'
 import './style.scss'
 
@@ -10,11 +10,19 @@ export type OptionSelectorProps = {
 
 export default ({ options = [], optionIdx = 0, setOptionIdx }: OptionSelectorProps) => {
   const setOpened = useContext(DrawerContext)?.setOpened
+  const selectRef = useRef<HTMLLIElement>()
+
+  useEffect(() => {
+    const height = selectRef.current.getBoundingClientRect().height
+
+    selectRef.current.style.top = `${height * optionIdx}px`
+  }, [optionIdx])
 
   return (
     <div className="option-selector">
       {options.length === 0 && <div>표시할 옵션이 없습니다.</div>}
       <ul>
+        <li ref={selectRef} className="select"></li>
         {options.map((x, idx) => (
           <li
             key={x}
