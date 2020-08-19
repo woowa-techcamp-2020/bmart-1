fc7ef61ac4a8bf9f24ab2a866486d3dd51a43cdd
eunjung <jenny.it.kr@gmail.com>
2020-08-19 21:58:06 +0900

    feat: add shrinking animation to CartItem (#57)
    중요! getBoundingClientRect()

--- src/pages/Cart/CartItem/index.tsx
+++ src/pages/Cart/CartItem/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { addCommaToPrice } from 'src/utils'
 import './style.scss'
 
@@ -11,7 +11,31 @@ export type CartItemProps = {
   imgV: string
 }
 
+function shrinkCartItem(item) {
+  const itemHeight = getComputedStyle(item).height
+
+  item.style.height = itemHeight
+
+  item.getBoundingClientRect()
+
+  item.classList.add('removed')
+}
+
 const CartItem: React.FC<CartItemProps> = ({ id, name, defaultPrice, price, quantity, imgV }) => {
+  const cartItemDelete = useRef<HTMLDivElement>()
+
+  useEffect(() => {
+    const { current: cartItemDeleteElem } = cartItemDelete
+
+    cartItemDeleteElem.addEventListener('click', (e) => {
+      const selectedItem = (e.target as HTMLElement).closest('.cart-item')
+
+      shrinkCartItem(selectedItem)
+      // TODO: 총계 새로 계산
+      // TODO: api call
+    })
+  }, [])
+
   return (
     <div className="cart-item" key={id}>
       <div className="cart-item-name">{name}</div>
@@ -20,7 +44,7 @@ const CartItem: React.FC<CartItemProps> = ({ id, name, defaultPrice, price, quan
         <div className="cart-item-info-right">
           <div className="minus-plus">MinusPlus</div>
           <div className="price">{addCommaToPrice(price)}</div>
-          <div className="delete">
+          <div className="delete" ref={cartItemDelete}>
             <div className="delete-icon"></div>
             <span className="delete-description">삭제</span>
           </div>
