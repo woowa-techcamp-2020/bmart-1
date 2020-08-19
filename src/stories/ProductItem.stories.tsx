256cec76b55f05c4974c22315300e7daef1513ce
eunjung <jenny.it.kr@gmail.com>
2020-08-19 17:16:40 +0900

    chore: add args for ProductItem storybook

--- src/stories/ProductItem.stories.tsx
+++ src/stories/ProductItem.stories.tsx
@@ -5,6 +5,15 @@ import ProductItem, { ProductItemProps } from '../components/ProductItem'
 export default {
   title: 'ProductItem',
   component: ProductItem,
+  args: {
+    id: 9529,
+    name: '[KF365] 애호박 1개',
+    defaultPrice: 4800,
+    price: 4580,
+    discount: 5,
+    imgV: 'https://img-cf.kurly.com/shop/data/goods/1539841569718l0.jpg',
+    isJjimmed: true,
+  },
 } as Meta
 
 const Template: Story<ProductItemProps> = (args) => <ProductItem {...args} />
