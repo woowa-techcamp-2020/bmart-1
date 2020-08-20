2563ff028c8e57fd4d1af61a3eff8a039784e4e2
jhaemin <io@jhaemin.com>
2020-08-21 02:46:58 +0900

    utils: get the latest state asynchronously

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,3 +1,21 @@
 export function addCommaToPrice(price: number) {
   return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
+
+/**
+ * Get the latest React state value from
+ * its state action function using functional updates
+ *
+ * @param setStateFunction React SetStateAction
+ */
+export default function getState<T = unknown>(
+  setStateFunction: (value: React.SetStateAction<T>) => void
+): Promise<T> {
+  return new Promise((resolve) => {
+    setStateFunction((prevState) => {
+      resolve(prevState)
+
+      return prevState
+    })
+  })
+}
