b848b80adc21f0eca7b238b7f5772a0c78727d7a
jhaemin <io@jhaemin.com>
2020-08-20 23:36:53 +0900

    chore: remove slot machine

--- src/pages/Bmart/Home/index.tsx
+++ src/pages/Bmart/Home/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import Carousel from 'src/components/Carousel'
-import SlotMachine from 'src/components/SlotMachine'
 import './style.scss'
 
 export type HomeProps = unknown
 
-const Home: React.FC<HomeProps> = (props) => {
+const Home: React.FC<HomeProps> = () => {
   return (
     <div className="home">
-      <SlotMachine>
-        <Carousel></Carousel>
-      </SlotMachine>
+      <Carousel></Carousel>
     </div>
   )
 }
