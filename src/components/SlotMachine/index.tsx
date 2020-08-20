dff438c0d1b8023ebb48726e073fcbdbb90b150d
agrajak <korbots@gmail.com>
2020-08-20 09:55:27 +0900

    feat: add SlotMachine component and style (#68)

---
+++ src/components/SlotMachine/index.tsx
@@ -0,0 +1,182 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './style.scss'
+
+export type SlotMachineProps = {
+  itemList: string[]
+}
+const NO_ACTION = 0
+const PULLING = 1
+const RELEASING = 2
+const WAITING = 3
+const RESETING = 4
+const MAX_PULL_LENGTH = 600
+const SHOW_DURATION = 800
+const ITEM_LENGTH = MAX_PULL_LENGTH / 8
+const ITEM_LIST = [
+  '김치찌개',
+  '샐러드',
+  '감자전',
+  '돈까스',
+  '삼겹살',
+  '천엽',
+  '엄마는외계인',
+  '짬뽕',
+  '고려국시',
+  '오호오호호',
+]
+
+const SlotMachine: React.FC<SlotMachineProps> = ({ itemList = ITEM_LIST }) => {
+  const action = useRef<number>(null)
+  const height = useRef<number>(0)
+  const pullable = useRef<HTMLDivElement>()
+  const slot = useRef<HTMLDivElement>()
+  const content = useRef<HTMLDivElement>()
+  const menu = useRef<HTMLSpanElement>()
+  const startY = useRef<number>(0)
+  const resetAt = useRef<number>(0)
+  const [item, setItem] = useState('')
+
+  useEffect(() => {
+    height.current = slot.current.getBoundingClientRect().height
+    slot.current.style.transform = `translatey(${-height.current}px)`
+    content.current.style.marginTop = `${-height.current}px`
+  }, [])
+
+  function pickRandomItem() {
+    return itemList[itemList.length * Math.random()]
+  }
+
+  function onCursorDown(y) {
+    if (pullable.current.scrollTop !== 0) return
+
+    action.current = PULLING
+    startY.current = y
+    slot.current.style.transitionDuration = null
+  }
+
+  function onCursorUp(y) {
+    if (action.current !== PULLING) return
+
+    action.current = RELEASING
+    moveVirtualDown(height.current)
+  }
+
+  function onCursorMove(y) {
+    if (action.current !== PULLING) return
+
+    const offsetY = y - startY.current
+
+    if (offsetY > 0) {
+      pullVirtualDown(offsetY)
+    }
+  }
+
+  function formula(r) {
+    return 1 - (1 - r) ** 3
+  }
+
+  function moveVirtualDown(offset) {
+    let duration = null
+
+    if (action.current === RELEASING) {
+      duration = '1.5s'
+    } else if (action.current === RESETING) {
+      duration = '0.3s'
+    }
+
+    slot.current.style.transitionDuration = duration
+    slot.current.style.marginTop = `${offset}px`
+  }
+
+  function pullVirtualDown(offset) {
+    let y = offset
+
+    if (y > MAX_PULL_LENGTH) {
+      y = MAX_PULL_LENGTH
+    } else {
+      y = MAX_PULL_LENGTH * formula(y / MAX_PULL_LENGTH)
+    }
+
+    y /= 2
+    moveVirtualDown(y)
+  }
+
+  function moveMenu(offset, N) {
+    menu.current.style.transform = `translatey(${offset}px)`
+    menu.current.style.opacity = `${offset < 0 ? (N - offset) / N : 1 - offset / N}`
+  }
+
+  function almostSame(a, b) {
+    return Math.abs(a * 10e5 - b * 10e5) < 10
+  }
+
+  let idx = null
+
+  function animate(t) {
+    if (action.current === PULLING || action.current === RELEASING) {
+      const y = parseFloat(getComputedStyle(slot.current).marginTop)
+      const targetY = parseFloat(slot.current.style.marginTop)
+      const NN = height.current,
+        N = NN / 2
+      const newIdx = Math.floor((y + N) / NN)
+
+      if (idx === null || newIdx !== idx) {
+        setItem(pickRandomItem())
+        idx = newIdx
+      }
+
+      moveMenu(((y + N) % NN) - N, N)
+
+      if (action.current == RELEASING && almostSame(y, targetY)) {
+        action.current = WAITING
+        resetAt.current = t + SHOW_DURATION
+      }
+    }
+
+    if (action.current === WAITING && resetAt.current < t) {
+      action.current = RESETING
+      moveVirtualDown(0)
+    }
+
+    window.requestAnimationFrame(animate)
+  }
+
+  window.requestAnimationFrame(animate)
+
+  return (
+    <div className="slot-machine">
+      <div
+        className="pullable"
+        ref={pullable}
+        onTouchStart={(event) => onCursorDown(getFirstTouchY(event))}
+        onTouchEnd={(event) => onCursorUp(getFirstTouchY(event))}
+        onTouchMove={(event) => onCursorMove(getFirstTouchY(event))}
+        onMouseDown={({ clientY }) => onCursorDown(clientY)}
+        onMouseUp={({ clientY }) => onCursorUp(clientY)}
+        onMouseMove={({ clientY }) => onCursorMove(clientY)}
+      >
+        <div className="slot" ref={slot}>
+          <span className="menu">{item}</span>
+          <span>땡겨요</span>
+        </div>
+        <div className="content" ref={content}>
+          pull to refresh
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function getFirstTouch(touchEvent: React.TouchEvent) {
+  return touchEvent.targetTouches[0] || touchEvent.changedTouches[0]
+}
+
+function getFirstTouchY(event) {
+  return getFirstTouch(event).clientY
+}
+
+function formula(r) {
+  return 1 - (1 - r) ** 3
+}
+
+export default SlotMachine
