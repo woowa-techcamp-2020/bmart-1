e8aa6ca2a7f06282170ea7f4ce16b1bb6ee295ac
eunjung <jenny.it.kr@gmail.com>
2020-08-21 21:06:08 +0900

    feat: update search markup

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProductItem from 'src/components/ProductItem'
 import './style.scss'
 
 export type SearchProps = unknown
 
 const Search: React.FC<SearchProps> = () => {
+  const [foundProducts, setFoundProducts] = useState([])
+
   return (
     <div className="search">
       <div className="search-input-wrapper">
@@ -11,7 +14,13 @@ const Search: React.FC<SearchProps> = () => {
         <input className="search-input" type="text" placeholder="검색"></input>
       </div>
 
-      <div className="search-result"></div>
+      <div className="search-results">
+        {foundProducts.map((product) => (
+          <div key={product.id} className="search-results-result">
+            <ProductItem {...product} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
