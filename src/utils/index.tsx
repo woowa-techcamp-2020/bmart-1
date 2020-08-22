a8d03a9556b94934182eeac72b436f540de4f565
jhaemin <io@jhaemin.com>
2020-08-22 12:58:00 +0900

    dev: extend $sel, $$sel to HTMLElement prototype

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,4 @@
-export function addCommaToPrice(price: number) {
+export function addCommaToPrice(price: number): string {
   return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
@@ -24,12 +24,24 @@ export default function getState<T = unknown>(
  * Returns a element matched with the given query
  */
 export function $sel<T extends HTMLElement>(query: string): T {
-  return document.querySelector<T>(query)
+  return ((this as HTMLElement) ?? document).querySelector<T>(query)
 }
 
 /**
  * Returns an array of elements matched with the given query
  */
 export function $$sel<T extends HTMLElement>(query: string): T[] {
-  return Array.from(document.querySelectorAll<T>(query))
+  return Array.from(
+    ((this as HTMLElement) ?? document).querySelectorAll<T>(query)
+  )
 }
+
+declare global {
+  interface HTMLElement {
+    $sel: typeof $sel
+    $$sel: typeof $$sel
+  }
+}
+
+HTMLElement.prototype.$sel = $sel
+HTMLElement.prototype.$$sel = $$sel
