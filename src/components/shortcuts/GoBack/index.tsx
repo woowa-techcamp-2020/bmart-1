1f7b2a9659dd4cf4feef17481b633eed1bfe789d
jhaemin <io@jhaemin.com>
2020-08-27 01:13:49 +0900

    feat: go back shortcut (#78)

---
+++ src/components/shortcuts/GoBack/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ChevronRightIcon from 'src/components/icons/ChevronRightIcon'
+import './style.scss'
+
+export type GoBackProps = unknown
+
+const GoBack: React.FC<GoBackProps> = () => {
+  return (
+    <div
+      className="go-back"
+      onClick={() => {
+        window.history.back()
+      }}
+    >
+      <div
+        style={{
+          transform: 'scaleX(-1)',
+          display: 'flex',
+          marginRight: '10px',
+        }}
+      >
+        <ChevronRightIcon width="0.5em" />
+      </div>
+      <span>뒤로 가기</span>
+    </div>
+  )
+}
+
+export default GoBack
