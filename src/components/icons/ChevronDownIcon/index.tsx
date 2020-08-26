cc2f11600a91f7227d21aabfe8f8dc56fda84e53
eunjung <jenny.it.kr@gmail.com>
2020-08-27 00:49:07 +0900

    chore: update style to support dark mode

--- src/components/icons/ChevronDownIcon/index.tsx
+++ src/components/icons/ChevronDownIcon/index.tsx
@@ -7,7 +7,7 @@ export type ChevronDownIconProps = {
 }
 
 const ChevronDownIcon: React.FC<ChevronDownIconProps> = ({
-  color = 'black',
+  color = 'var(--distinct)',
   width = '17px',
 }) => {
   return (
