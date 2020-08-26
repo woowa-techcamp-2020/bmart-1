c078e3247efc0fda9e684727eac2834856c6a333
agrajak <korbots@gmail.com>
2020-08-26 18:55:20 +0900

    feat: add LoginContext for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import {
   BrowserRouter as Router,
   Redirect,
@@ -8,30 +8,46 @@ import {
 import './app.style.scss'
 import Bmart from './pages/Bmart'
 import Jjims from './pages/Jjims'
+import { Dispatcher } from './types/react-helper'
+
+export const LoginContext = createContext<{
+  isLogged: boolean
+  setIsLogged: Dispatcher<boolean>
+}>(undefined)
 
 const App: React.FC = () => {
+  const [isLogged, setIsLogged] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setIsLogged(true)
+    }
+  }, [])
+
   return (
-    <Router>
-      <div className="app">
-        <Switch>
-          <Route path="/" exact>
-            <Bmart />
-          </Route>
-          <Route path="/sale" exact>
-            <Bmart />
-          </Route>
-          <Route path="/me" exact>
-            <Bmart />
-          </Route>
-          <Route path="/jjims">
-            <Jjims />
-          </Route>
-          <Route path="/verified">
-            <Redirect to="/"></Redirect>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <LoginContext.Provider value={{ isLogged, setIsLogged }}>
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route path="/" exact>
+              <Bmart />
+            </Route>
+            <Route path="/sale" exact>
+              <Bmart />
+            </Route>
+            <Route path="/me" exact>
+              <Bmart />
+            </Route>
+            <Route path="/jjims">
+              <Jjims />
+            </Route>
+            <Route path="/verified">
+              <Redirect to="/"></Redirect>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </LoginContext.Provider>
   )
 }
 
