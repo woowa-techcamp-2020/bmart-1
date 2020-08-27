18291c62922a2724350358f4374b49d5a4c389ed
agrajak <korbots@gmail.com>
2020-08-27 14:22:16 +0900

    feat: add page for Addresses

---
+++ src/pages/Addresses/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.scss'
+
+export type AddressesProps = unknown
+
+const Addresses: React.FC<AddressesProps> = () => {
+  return <div className="addresses"></div>
+}
+
+export default Addresses
