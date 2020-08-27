2725b55bf98086b5280a54561e433b7aadd8314c
agrajak <korbots@gmail.com>
2020-08-28 02:05:59 +0900

    feat: add lazy-loding in TopicContainer

--- src/pages/Bmart/Home/TopicContainer/index.tsx
+++ src/pages/Bmart/Home/TopicContainer/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { getProductsByTopic } from 'src/apis'
 import ProductItem from 'src/components/ProductItem'
 import { ProductWithJjimmed } from 'src/types/api'
+import { useLazy } from 'src/utils/hooks'
 import './style.scss'
 
 export type TopicContainerProps = {
@@ -14,17 +15,16 @@ const TopicContainer: React.FC<TopicContainerProps> = ({
   type = 'new',
 }) => {
   const [products, setProducts] = useState<ProductWithJjimmed[]>([])
+  const [isLoading, setLoading] = useState(true)
 
   async function loadProducts() {
     const newProducts = (await getProductsByTopic(type)) as ProductWithJjimmed[]
 
     setProducts(newProducts)
+    setLoading(false)
   }
 
-  useEffect(() => {
-    loadProducts()
-  }, [])
-
+  useLazy(loadProducts)
   const [scrollEnd, setScrollEnd] = useState<'left' | 'right' | 'middle'>(
     'left'
   )
@@ -48,9 +48,15 @@ const TopicContainer: React.FC<TopicContainerProps> = ({
             }
           }}
         >
-          {products.map((x, i) => (
-            <ProductItem key={i} {...x}></ProductItem>
-          ))}
+          {isLoading
+            ? new Array(12)
+                .fill(undefined)
+                .map((x, i) => (
+                  <ProductItem isSkeleton={true} key={`s${i}`}></ProductItem>
+                ))
+            : products.map((x, i) => (
+                <ProductItem key={i} {...x}></ProductItem>
+              ))}
           <div className="dummy" />
         </div>
 
