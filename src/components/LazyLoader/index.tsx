6f45b9e4bcf05dbf3e501610b48dde233422ba90
agrajak <korbots@gmail.com>
2020-08-28 01:07:46 +0900

    chore: add isRemovable props in LazyLoader

--- src/components/LazyLoader/index.tsx
+++ src/components/LazyLoader/index.tsx
@@ -1,21 +1,31 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './style.scss'
 
-export type LazyLoaderProps = unknown
+export type LazyLoaderProps = {
+  isRemovable?: boolean
+}
 
-const LazyLoader: React.FC<LazyLoaderProps> = ({ children }) => {
+const LazyLoader: React.FC<LazyLoaderProps> = ({
+  isRemovable = false,
+  children,
+}) => {
   const sentinelRef = useRef<HTMLDivElement>()
   const observer = useRef<IntersectionObserver>()
   const [isLoaded, setLoaded] = useState<Boolean>(false)
 
   useEffect(() => {
-    observer.current = new IntersectionObserver((entries) => {
-      const entry = entries.find(() => true)
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        const entry = entries.find(() => true)
 
-      if (entry.isIntersecting) {
-        setLoaded(true)
-      }
-    })
+        if (entry.intersectionRatio > 0.01) {
+          setLoaded(true)
+        } else {
+          isRemovable && setLoaded(false)
+        }
+      },
+      { threshold: [0, 0.01, 0.1] }
+    )
     observer.current.observe(sentinelRef.current)
   }, [])
 
