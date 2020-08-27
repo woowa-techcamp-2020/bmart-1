df767b6b27a2b2d941e3880ee36da3dc60ab0b8c
agrajak <korbots@gmail.com>
2020-08-27 16:04:53 +0900

    chore: add props (isDefault, onEdit, onDelete) for AddressItem

--- src/pages/Addresses/AddressItem/index.tsx
+++ src/pages/Addresses/AddressItem/index.tsx
@@ -1,17 +1,27 @@
+import $ from 'classnames'
 import React from 'react'
 import './style.scss'
 
 export type AddressItemProps = {
   address1: string
   address2: string
+  isDefault?: boolean
+  onEdit?: () => void
+  onDelete?: () => void
 }
 
-const AddressItem: React.FC<AddressItemProps> = ({ address1, address2 }) => {
+const AddressItem: React.FC<AddressItemProps> = ({
+  address1,
+  address2,
+  isDefault = false,
+  onEdit,
+  onDelete,
+}) => {
   return (
-    <div className="address-item">
+    <div className={$(['address-item', { default: isDefault }])}>
       <div className="row">
-        <a>수정</a>
-        <a>삭제</a>
+        <a onClick={() => onEdit && onEdit()}>수정</a>
+        <a onClick={() => onDelete && onDelete()}>삭제</a>
       </div>
       <div className="address1">{address1}</div>
       <div className="address1">{address2}</div>
