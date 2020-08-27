6c49e13d682cdc5dcd58efc890709c7097e09d39
jhaemin <io@jhaemin.com>
2020-08-27 11:51:27 +0900

    feat: scrollable topic container (#117)

--- src/pages/Bmart/Home/TopicContainer/index.tsx
+++ src/pages/Bmart/Home/TopicContainer/index.tsx
@@ -25,15 +25,37 @@ const TopicContainer: React.FC<TopicContainerProps> = ({
     loadProducts()
   }, [])
 
+  const [scrollEnd, setScrollEnd] = useState<'left' | 'right' | 'middle'>(
+    'left'
+  )
+
   return (
     <div className="topic-container">
       <div className="title">{title}</div>
       <div className="scroll-container">
-        <div className="container">
+        <div
+          className="container"
+          onScroll={(e) => {
+            if (
+              e.currentTarget.scrollLeft + e.currentTarget.clientWidth >=
+              e.currentTarget.scrollWidth
+            ) {
+              setScrollEnd('right')
+            } else if (e.currentTarget.scrollLeft === 0) {
+              setScrollEnd('left')
+            } else {
+              setScrollEnd('middle')
+            }
+          }}
+        >
           {products.map((x, i) => (
             <ProductItem key={i} {...x}></ProductItem>
           ))}
+          <div className="dummy" />
         </div>
+
+        {scrollEnd !== 'left' && <div className="shadow left" />}
+        {scrollEnd !== 'right' && <div className="shadow right" />}
       </div>
     </div>
   )
